435b58df5ec4081fff52ba28a36e313746a53eaa
Somogyi Krisztian <krisztian@somogyi.xyz>
2021-04-19 18:52:55 +0200

    Fix gitarok.php

--- webshop/gitarok.php
+++ webshop/gitarok.php
@@ -12,48 +12,23 @@
 <h1><u>Gitárok</u></h1>
 <?php
     
-    // csatlakozás az adatbázishoz
-    $conn = mysqli_connect('localhost', 'root','') or die("Hibás csatlakozás!");
-
-
-    // a karakterek helyes megjelenítése miatt be kell állítani a karakterkódolást!
-    mysqli_query($conn, 'SET NAMES UTF-8');
-    mysqli_query($conn, "SET character_set_results=utf8");
-    mysqli_set_charset($conn, 'utf-8');
-         
-    if ( mysqli_select_db($conn, 'webshop2') ) {    // ha sikeres az adatbázis kiválasztása    
-        
-        
-        $sql = "SELECT gitar.nev , gitar.ar, gitar.leiras, gitar.kep FROM `gitar` 
-
-"; // ez csak egy string, még nem hajtódik végre
-        $res = mysqli_query($conn, $sql) or die ('Hibás utasítás!'); // végrehajtjuk az SQL utasítást
-          
+    $db = new SQLite3('./guitarshop.sqlite');
+    $results = $db->query("select * from termekek");
         // html táblázatként íratjuk ki;
         $kimenet="";
-		while ($sor=mysqli_fetch_assoc($res)){
-			$kimenet.="
-						<div id='fo'>
-						<h2>{$sor['nev']}</h2>
-						<img id='kep' src={$sor['kep']}>
-						<p id={$sor['leiras']}>
-    
-
-						</p>
-						<p id={$sor['ar']}></p>
-						<form>
-   
-						<input id='db' type='text' name='darab' value='0'><button>Kosárba</button>
-						</form>
-						</div>";}
-		// print_r($sor);
+        while ($row= $results->fetchArray()){
+	$kimenet.="
+	<div id='fo'>
+	<h2>{$row['nev']}</h2>
+	<img id='kep' src={$row['kep']}>
+	<p id={$row['leiras']}>
+    	</p>
+	<p id={$row['ar']}></p>
+	<form>
+   	<input id='db' type='text' name='darab' value='0'><button>Kosárba</button>
+        </form>
+	</div>";}
         
-        mysqli_free_result($res);    // felszabadítjuk a lefoglalt memóriát
-    } else {                                    // nem sikerült csatlakozni az adatbázishoz
-        die('Nem sikerült csatlakozni az adatbázishoz.');
-    }
- 
-    mysqli_close($conn); // lezárjuk az adatbázis-kapcsolatot
 ?> 
 
 
