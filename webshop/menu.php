9bf4a82b55a506f1477bc2704771ece1ac58e471
huba855 <73494026+huba855@users.noreply.github.com>
2021-04-26 18:07:11 +0200

    Create menu.php

---
+++ webshop/menu.php
@@ -0,0 +1,11 @@
+
+
+ <div id="menu">
+    
+	<a href="gitarok.php" class="alap gitarok"><u>Gitárok</u></a>
+    <a href="bass.php" class="alap gitarok"><u>Basszus gitárok</u></a>
+    <a href="amps.php" class="alap erositok"><u>Erősítők</u></a>
+    <a href="misc.php" class="alap kiegeszitok"><u>Kiegészítők</u></a>
+    <a href="shoppingcart.php" class="alap kosar"><u>Kosár</u></a>
+	
+  </div>
