2279369c5644d3050342e9d3ec8c1406299e8d3c
huba855 <73494026+huba855@users.noreply.github.com>
2021-04-26 18:23:49 +0200

    Update menu.php

--- webshop/menu.php
+++ webshop/menu.php
@@ -6,6 +6,6 @@
     <a href="bass.php" class="alap gitarok"><u>Basszus gitárok</u></a>
     <a href="amps.php" class="alap erositok"><u>Erősítők</u></a>
     <a href="misc.php" class="alap kiegeszitok"><u>Kiegészítők</u></a>
-    <a href="shoppingcart.php" class="alap kosar"><u>Kosár</u></a>
+    <a href="shopping_cart.php" class="alap kosar"><u>Kosár</u></a>
 	
   </div>
