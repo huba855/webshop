06c46e6df1c3b6adc6cd142e0bfec0549ff088f1
K <sup3rn4tural@gmail.com>
2021-04-27 19:53:34 +0200

    Code cleanup

--- webshop/guitar.php
+++ webshop/guitar.php
@@ -3,21 +3,21 @@
 <?php
     
     $results = $db->query("select * from termekek where typeId = 0");
-        // html táblázatként íratjuk ki;
-        $kimenet="";
-        while ($row= $results->fetchArray()){
-	$kimenet.="
-	<div id='fo'>
-	<h2>{$row['nev']}</h2>
-	<img id='kep' src=./static/{$row['kep']}>
-	<p id={$row['leiras']}>
-    	</p>
-	<p id={$row['ar']}></p>
-	<form method='post' action='add_to_cart.php'>
-   	<input id='db' type='text' name='darab' value='0'><button>Kosárba</button>
-        <input type='hidden' name='id' value={$row['id']}>
-        </form>
-	</div>";}
+        $kimenet = "";
+        while ($row= $results->fetchArray())
+		{
+			$kimenet.="
+				<div>
+				<h2>{$row['nev']}</h2>
+				<img id='kep' src=./static/{$row['kep']}>
+				<p id={$row['leiras']}></p>
+				<p id={$row['ar']}></p>
+				<form method='post' action='add_to_cart.php'>
+					<input id='db' type='text' name='darab' value='1'><button>Kosárba</button>
+					<input type='hidden' name='id' value={$row['id']}>
+				</form>
+				</div>";
+	}
         
 print $kimenet; ?>
 </div>
