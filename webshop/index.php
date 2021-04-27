c274bb7f295d534162ab9308df02fdd54905db5b
K <sup3rn4tural@gmail.com>
2021-04-27 19:49:34 +0200

    Fix warning on index.php

--- webshop/index.php
+++ webshop/index.php
@@ -26,7 +26,10 @@
     } 
 
     include './menu.php';
-    $page = $_GET["page"];
+    if(isset($_GET["page"]))
+      $page = $_GET["page"];
+    else
+      $page = "home";
     switch($page)
     {
       case "guitar":
