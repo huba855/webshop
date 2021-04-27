9f3259c3780274dab8a7c2397a478b524f5c8bcf
K <sup3rn4tural@gmail.com>
2021-04-27 19:59:44 +0200

    Remove unused or bad code

--- webshop/bass.php
+++ webshop/bass.php
@@ -1,45 +1,23 @@
-<?php 
-include './cart.php';
-?>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset='utf-8'>
-
-<title>Bass guitars</title>
-
-<link rel='stylesheet' href='./static/style2.css'>
-
-</head>
-<body>
-	<?php include './menu.php'; ?>
-<h1><u>Bass guitars</u></h1>
+<div>
+<?php if(isset($_SESSION["message"])) { print $_SESSION["message"]; unset($_SESSION["message"]);}?>
 <?php
     
     $results = $db->query("select * from termekek where typeId = 1");
-        // html táblázatként íratjuk ki;
-        $kimenet="";
-        while ($row= $results->fetchArray()){
-	$kimenet.="
-	<div id='fo'>
-	<h2>{$row['nev']}</h2>
-	<img id='kep' src=./static/{$row['kep']}>
-	<p id={$row['leiras']}>
-    	</p>
-	<p id={$row['ar']}></p>
-	<form method='post' action='add_to_cart.php'>
-   	<input id='db' type='text' name='darab' value='0'><button>Kosárba</button>
-        <input type='hidden' name='id' value={$row['id']}>
-        </form>
-	</div>";}
+        $kimenet = "";
+        while ($row= $results->fetchArray())
+		{
+			$kimenet.="
+				<div>
+				<h2>{$row['nev']}</h2>
+				<img id='kep' src=./static/{$row['kep']}>
+				<p id={$row['leiras']}></p>
+				<p id={$row['ar']}></p>
+				<form method='post' action='add_to_cart.php'>
+					<input id='db' type='text' name='darab' value='1'><button>Kosárba</button>
+					<input type='hidden' name='id' value={$row['id']}>
+				</form>
+				</div>";
+	}
         
-?> 
-
-
-
-
-
-
-<?php print $kimenet; ?>
-</body>
-</html>
+print $kimenet; ?>
+</div>
