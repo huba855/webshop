6d1ce567241a6db13505215e1f57f94e074fddd1
K <sup3rn4tural@gmail.com>
2021-04-27 19:51:32 +0200

    Fix menu.php

--- webshop/menu.php
+++ webshop/menu.php
@@ -1,11 +1,7 @@
-
-
- <div id="menu">
-    
-	<a href="gitarok.php" class="alap gitarok"><u>Gitárok</u></a>
-    <a href="bass.php" class="alap gitarok"><u>Basszus gitárok</u></a>
-    <a href="amps.php" class="alap erositok"><u>Erősítők</u></a>
-    <a href="misc.php" class="alap kiegeszitok"><u>Kiegészítők</u></a>
-    <a href="shopping_cart.php" class="alap kosar"><u>Kosár</u></a>
-	
-  </div>
+<div id="menu">
+	<a href="index.php?page=guitar" class="alap gitarok"><u>Gitárok</u></a>
+  <a href="index.php?page=bass" class="alap gitarok"><u>Basszus gitárok</u></a>
+  <a href="index.php?page=amp" class="alap erositok"><u>Erősítők</u></a>
+  <a href="index.php?page=misc" class="alap kiegeszitok"><u>Kiegészítők</u></a>
+  <a href="index.php?page=cart" class="alap kosar"><u>Kosár</u></a>
+</div>
