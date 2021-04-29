4456f067370d4f902c1deaec5b9ff0a1bd0fab03
huba855 <73494026+huba855@users.noreply.github.com>
2021-04-29 23:21:26 +0200

    Update menu.php

--- webshop/menu.php
+++ webshop/menu.php
@@ -1,6 +1,6 @@
 <div class="topnav">
 	<a href="index.php?page=guitar" class="alap gitarok"><u>Gitárok</u></a>
-  <a href="index.php?page=bass" class="alap gitarok"><u>Basszus gitárok</u></a>
+  <a href="index.php?page=bass" class="alap gitarok"><u>Basszusgitárok</u></a>
   <a href="index.php?page=amp" class="alap erositok"><u>Erősítők</u></a>
   <a href="index.php?page=misc" class="alap kiegeszitok"><u>Kiegészítők</u></a>
   <a href="index.php?page=cart" class="alap kosar"><u>Kosár</u></a>
